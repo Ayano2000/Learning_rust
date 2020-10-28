7249611b0797755706efa6150b04fe66095519ff
Arata Yano <aratayano00@gmail.com>
2020-10-28 20:04:35 +0200

    done for now

--- rust-gentle-intro/Pointers/Pointers.rs
+++ rust-gentle-intro/Pointers/Pointers.rs
@@ -1,1 +1,11 @@
-a
+// function takes a reference to the variable you want it to change
+fn modifies(x: &mut f64) {
+    // dereference with "*"
+    *x = 22.03
+}
+
+fn main() {
+    let mut res = 0.0;
+    modifies(&mut res);
+    println!("res is {}", res);
+}
